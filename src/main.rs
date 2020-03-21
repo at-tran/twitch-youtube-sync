5a80b48bb41e3ab51ed93f900ad1024ba2b8d3fb
Anh Tuan Tran <nhanhnhanh17399@gmail.com>
2020-03-20 22:39:04 -0400

    YouTube upload API WIP

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,20 @@
 use regex::Regex;
 use reqwest::blocking;
+use reqwest::header::CONTENT_TYPE;
+use serde_json::{json, Value};
 use serde_urlencoded;
 use std::collections::HashMap;
 use std::fs;
 use std::process::{Command, Stdio};
 
 fn main() {
-    let video_id = "566138609";
-    let (token, sig) = get_access_token(&get_client_id(), video_id);
-    println!("{}\n{}", token, sig);
-    let m3u8_url = get_m3u8_url(video_id, &token, &sig);
-    println!("{}", m3u8_url);
-    download_video(video_id, &m3u8_url);
+    let video_id = "567314621";
+    // let (token, sig) = get_access_token(&get_client_id(), video_id);
+    // let m3u8_url = get_m3u8_url(video_id, &token, &sig);
+    // download_video(video_id, &m3u8_url);
+    let (client_id, client_secret) = get_client_secrets();
+    let auth_token = get_auth_token(&client_id);
+    // get_upload_session_uri(&api_key, &video_id);
 }
 
 fn get_client_id() -> String {
@@ -61,7 +64,72 @@ fn download_video(video_id: &str, m3u8_url: &str) {
             "-y",
             &format!("videos/{}.mp4", video_id),
         ])
+        .stdout(Stdio::piped())
         .stderr(Stdio::piped())
         .status()
         .unwrap();
+}
+
+fn get_client_secrets() -> (String, String) {
+    let filepath = "client_secrets.json";
+    let file = fs::File::open(filepath).expect(&format!("Cannot find {}", filepath));
+    let json: Value =
+        serde_json::from_reader(file).expect(&format!("{} does not contain valid json", filepath));
+
+    (
+        get_string_from_value(&json["web"]["client_id"]),
+        get_string_from_value(&json["web"]["client_secret"]),
+    )
+}
+
+fn get_string_from_value(v: &Value) -> String {
+    if let Value::String(s) = &v {
+        s.clone()
+    } else {
+        panic!("{} is not a string", v)
+    }
+}
+
+fn get_auth_token(client_id: &str) -> String {
+    let client = reqwest::blocking::Client::new();
+    let res = client
+        .post("https://oauth2.googleapis.com/device/code")
+        .header(CONTENT_TYPE, "application/x-www-form-urlencoded")
+        .body(format!(
+            "client_id={}&scope=https://www.googleapis.com/auth/youtube.upload",
+            client_id
+        ))
+        .send()
+        .unwrap()
+        .text()
+        .unwrap();
+    println!("{:?}", res);
+    "haha".to_string()
+}
+
+fn get_upload_session_uri(api_key: &str, video_id: &str) {
+    let client = reqwest::blocking::Client::new();
+    let req_body = json!({
+      "snippet": {
+        "title": video_id,
+        "description": "This is a description of my video",
+        "tags": ["cool", "video", "more keywords"],
+        "categoryId": 22
+      },
+      "status": {
+        "privacyStatus": "unlisted",
+        "embeddable": true,
+        "license": "youtube"
+      }
+    })
+    .to_string();
+    let res = client
+        .post("https://www.googleapis.com/upload/youtube/v3/videos")
+        .bearer_auth(api_key)
+        .header(CONTENT_TYPE, "application/json; charset=UTF-8")
+        .header("X-Upload-Content-Length", "300")
+        .header("X-Upload-Content-Type", "video/*")
+        .body(req_body)
+        .send();
+    println!("{:?}", res);
 }
